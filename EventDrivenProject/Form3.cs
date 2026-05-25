47ed5726b769084b72b2b79195f6d723995b3f57
haziq <haziq.irfann04@gmail.com>
2026-05-25 22:55:11 +0800

    validate login&signup

--- EventDrivenProject/Form3.cs
+++ EventDrivenProject/Form3.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ namespace EventDrivenProject
 {
     public partial class Form3 : Form
     {
+        public static string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;
+                                      AttachDbFilename=C:\Users\haziq\Downloads\EventDrivenProject\EventDrivenProject\TicketCinema.mdf;
+                                      Integrated Security = True; TrustServerCertificate=True; Encrypt=False";
+        SqlConnection con = new SqlConnection(conn);
         public Form3()
         {
             InitializeComponent();
@@ -19,6 +24,17 @@ namespace EventDrivenProject
 
         private void button1_Click(object sender, EventArgs e)
         {
+            con.Open();
+
+            string query = "select Count(*) from [User] where Email = @email";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue(@"email", textBox3.Text);
+
+            int count = (int)cmd.ExecuteScalar();
+
+            con.Close();
+
             if (string.IsNullOrEmpty(textBox1.Text))
             {
                 MessageBox.Show("First name can't be null", "Error", MessageBoxButtons.OK);
@@ -32,6 +48,11 @@ namespace EventDrivenProject
             else if (string.IsNullOrEmpty(textBox3.Text))
             {
                 MessageBox.Show("Email can't be null", "Error", MessageBoxButtons.OK);
+                return;
+            }
+            else if (count == 1)
+            {
+                MessageBox.Show("Email already exist", "Error", MessageBoxButtons.OK);
                 return;
             }
             else if (string.IsNullOrEmpty(textBox4.Text))
@@ -49,6 +70,21 @@ namespace EventDrivenProject
                 MessageBox.Show("Confirm password not same with password", "Error", MessageBoxButtons.OK);
                 return;
             }
+            con.Open();
+
+            string insert = "insert into [User] (FirstName, LastName, Email, Password)" + "values(@fn, @ln, @email, @pass)";
+            SqlCommand cmd2 = new SqlCommand(insert, con);
+
+            cmd2.Parameters.AddWithValue("@fn", textBox1.Text);
+            cmd2.Parameters.AddWithValue("@ln", textBox2.Text);
+            cmd2.Parameters.AddWithValue("@email", textBox3.Text);
+            cmd2.Parameters.AddWithValue("@pass", textBox4.Text);
+
+            cmd2.ExecuteNonQuery();
+
+            con.Close();
+
+            MessageBox.Show("User registered successfully", "Success", MessageBoxButtons.OK);
             this.Close();
             Form2 f2 = new Form2();
             f2.Show();
